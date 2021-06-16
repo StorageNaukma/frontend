87fbc877d3c4ac08d75c6379693dc994bf9f4388
Nikita Maslov <maslov.n.e@gmail.com>
2021-06-16 12:29:07 +0300

    remove unused strings and left unused files

--- src/js/modules/fixModals.js
+++
@@ -1,9 +0,0 @@
-const fixModals = () => {
-    document.querySelectorAll(".modal").forEach((modal) => {
-       modal.addEventListener("show.bs.modal", () => {
-               // document.querySelector(".modal-backdrop").remove();
-       });
-    });
-};
-
-export default fixModals;
